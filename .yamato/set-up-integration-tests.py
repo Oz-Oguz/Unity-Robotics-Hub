94fafa9a10d3ffd06c3b76e16707df3a441a079e
Devin Miller (Unity) <devin.miller@unity3d.com>
2021-05-21 11:37:24 -0700

    Finished up Integration Test automation - ready for validation

--- .yamato/set-up-integration-tests.py
+++ .yamato/set-up-integration-tests.py
@@ -1,29 +1,33 @@
 import os
 import shutil
 import glob
-import yaml
+from collections import OrderedDict
+from unityparser import UnityDocument
+
+# NOTE: This must match the flag defined in the Unity Integration Tests
+_INTEGRATION_TEST_DEFINE = "INTEGRATION_TEST"
 
 # This script is executed inside of a bokken image in order to automate the manual steps a user would
 # perform when going through the tutorials. This allows us to perform integration tests on the expected final
 # state of a tutorial project.
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
-root = os.path.join(script_dir, "..", "tutorials", "pick_and_place")
-external_scripts = os.path.join(root, "Scripts")
-project = os.path.join(root, "PickAndPlaceProject")
-project_scripts = os.path.join(project, "Assets", "Scripts")
-project_settings = os.path.join(project, "ProjectSettings", "ProjectSettings.asset")
 
-scripts_to_move = glob.glob(os.path.join(external_scripts, "*.cs"))
+script_dir = os.path.dirname(os.path.realpath(__file__))
+root_dir = os.path.join(script_dir, "..", "tutorials", "pick_and_place")
+external_scripts_dir = os.path.join(root_dir, "Scripts")
+project_dir = os.path.join(root_dir, "PickAndPlaceProject")
+project_scripts_dir = os.path.join(project_dir, "Assets", "Scripts")
+project_settings_file = os.path.join(project_dir, "ProjectSettings", "ProjectSettings.asset")
+
+scripts_to_move = glob.glob(os.path.join(external_scripts_dir, "*.cs"))
 for external_script in scripts_to_move:
     script_name = os.path.basename(external_script)
-    shutil.copyfile(external_script, os.path.join(project_scripts, script_name))
+    shutil.copyfile(external_script, os.path.join(project_scripts_dir, script_name))
 
-with open(project_settings, "r") as f:
-    settings_yaml = yaml.full_load(f)
-
-settings_yaml["PlayerSettings"]["scriptingDefineSymbols"].append(["INTEGRATION_TESTS"])
-
-with open(project_settings, "w") as f:
-    yaml.dump(settings_yaml, f)
-
+project_settings_asset = UnityDocument.load_yaml(project_settings_file)
+scripting_defines = project_settings_asset.entry.scriptingDefineSymbols  # type: OrderedDict
+if scripting_defines[1]:
+    scripting_defines[1] += f";{_INTEGRATION_TEST_DEFINE}"
+else:
+    scripting_defines[1] = _INTEGRATION_TEST_DEFINE
+project_settings_asset.dump_yaml()
