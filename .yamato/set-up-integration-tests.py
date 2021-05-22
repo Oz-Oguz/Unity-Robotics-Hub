a6c0c468ef2c418a548d623f56871abc3aeea0fc
Devin Miller (Unity) <devin.miller@unity3d.com>
2021-05-21 17:09:07 -0700

    Reverting to using sed instead of unityparser for settings changes

--- .yamato/set-up-integration-tests.py
+++ .yamato/set-up-integration-tests.py
@@ -24,10 +24,10 @@ for external_script in scripts_to_move:
     script_name = os.path.basename(external_script)
     shutil.copyfile(external_script, os.path.join(project_scripts_dir, script_name))
 
-project_settings_asset = UnityDocument.load_yaml(project_settings_file)
-scripting_defines = project_settings_asset.entry.scriptingDefineSymbols  # type: OrderedDict
-if scripting_defines[1]:
-    scripting_defines[1] += f";{_INTEGRATION_TEST_DEFINE}"
-else:
-    scripting_defines[1] = _INTEGRATION_TEST_DEFINE
-project_settings_asset.dump_yaml()
+#project_settings_asset = UnityDocument.load_yaml(project_settings_file)
+#scripting_defines = project_settings_asset.entry.scriptingDefineSymbols  # type: OrderedDict
+#if scripting_defines[1]:
+#    scripting_defines[1] += f";{_INTEGRATION_TEST_DEFINE}"
+#else:
+#    scripting_defines[1] = _INTEGRATION_TEST_DEFINE
+#project_settings_asset.dump_yaml()
